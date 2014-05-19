73465e9f373be44f7be9d1d334c60b470dd8f819
Laurent Szyster <laurentszyster@gmail.com>
2014-05-19 23:10:38 +0200

    added semaphore to the loop, a work in progress

--- src/loop_json.php
+++ src/loop_json.php
@@ -1,50 +1,32 @@
 <?php
 
-/**
- * Bend it like Joe.
- *
- *     unframed_loop($verify_key, $poll_messages, $cast_url);
- *
- * Receive a JSON message POSTed with HTTP/1.0.
- *
- * Verify a key with a function or fail.
- *
- * Poll messages from a function.
- *
- * Cast messages to as many folded processes for the same URL.
- *
- * Commit any results to a function or sleep a while if the queue is empty. 
- *
- * Then loop, cast to self with a new time and key.
- *
- * POST /loop.php { "time": 0, "processes": 4, "key": "..." }
- * POST /cast.php { "time": 0, "messages": [...] }
- *
- * Note that a cast does not require a key but a local origin.
- */
-
 require_once(dirname(__FILE__).'/fold_json.php');
 
-define('UNFRAMED_LOOP_TIME', microtime(TRUE));
-
 /**
- * Call the loop's own URL if `UNFRAMED_LOOP_CONTINUE` is defined and true. 
+ * Cast UNFRAMED_LOOP_MESSAGE to the loop's own URL 
+ * if the semaphore file UNFRAMED_LOOP_CONTINUE exists. 
  */
 function unframed_loop_continue () {
-    if (defined('UNFRAMED_LOOP_CONTINUE') && UNFRAMED_LOOP_CONTINUE) {
-        unframed_cast(unframed_cast_url(), array(
-            'time' => UNFRAMED_LOOP_TIME, 
-            'connection' => connection_status()
-            ));
+    if (file_exists(UNFRAMED_LOOP_CONTINUE)) {
+        unframed_cast(unframed_cast_url(), UNFRAMED_LOOP_MESSAGE);
     }
 }
 
 /**
- * Accept a JSON request POSTed, fail or handle it with $fun and if
- * the return value is not negative make sure script's own URL is called
- * before this instance terminates.
+ * Fail or receive a JSON cast, if the result of $fun($json) is not NULL, sleep 
+ * $seconds - 30 by default - and then either cast the result to itself or delete
+ * the $semaphore to stop the loop.
+ *
+ * Note that the cast will silently fail and this instance of the loop will stop
+ * if the semaphore was touched sooner than $seconds before now, ie: if another
+ * instance of the loop is running.
+ *
+ * @param callable $fun to apply to update the loop's $state
+ * @param int $seconds to sleep between two cast to the loop
+ * @param string $semaphore the name of the semaphore file for this loop
  */
-function unframed_loop ($fun) {
+function unframed_loop_json ($fun, $seconds=30, $semaphore='.unframed_loop') {
+    define('UNFRAMED_LOOP_CONTINUE', $semaphore);
     try {
         $json = unframed_cast_receive();
     } catch (Unframed $error) {
@@ -52,14 +34,30 @@ function unframed_loop ($fun) {
         echo $e->getMessage(), "\n";
     }
     if (isset($json)) {
-        register_shutdown_function('unframed_loop_continue');
         unframed_cast_ok();
-        $interval = unframed_call($fun, array());
-        if ($interval < 0) {
-            define('UNFRAMED_LOOP_CONTINUE', FALSE);
-        } else {
-            define('UNFRAMED_LOOP_CONTINUE', TRUE);
-            sleep($interval);
+        if (!(file_exists($semaphore) && (time() - filemtime($semaphore) < $seconds))) {
+            register_shutdown_function('unframed_loop_continue');
+            touch($semaphore);
+            $state = unframed_call($fun, array($json));
+            if ($state == NULL) {
+                unlink($semaphore);
+            } else {
+                define('UNFRAMED_LOOP_MESSAGE', $state);
+                sleep($seconds);
+            }
         }
+    }
+}
+
+function unframed_loop_start ($message, $uri=NULL, $semaphore='.unframed_loop') {
+    if (!file_exists($semaphore)) {
+        touch($semaphore);
+        unframed_cast(unframed_cast_url($uri), $message);
+    }
+}
+
+function unframed_loop_stop ($semaphore='.unframed_loop') {
+    if (file_exists($semaphore)) {
+        unlink($semaphore);
     }
 }
