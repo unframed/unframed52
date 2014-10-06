84a4f1180b610d013722cd7ff91817d0d52f61a5
Laurent Szyster <laurentszyster@gmail.com>
2014-10-06 09:43:48 +0200

    added unframed_cast_encoded and a test suite that configures

--- src/cast_json.php
+++ src/cast_json.php
@@ -9,29 +9,24 @@ unframed_no_script(__FILE__);
 /**
  * Return the complete URL of the given $uri or the script's URL if
  * $uri is NULL.
- * 
+ *
  * @param string $uri to complete, NULL by default
  */
 function unframed_cast_url($uri=NULL) {
-    $url = parse_url($uri==NULL?$_SERVER['REQUEST_URI']:$uri);
-    return (
-        "http".(!empty($_SERVER['HTTPS'])?"s":"")."://"
-        .$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT']
-        .$url['path']
-        );
+    $url = parse_url($uri===NULL?$_SERVER['REQUEST_URI']:$uri);
+    return unframed_site_url().$url['path'];
 }
 
 /**
- * POST a JSON $request to a $url with HTTP/1.0, return the response's body.
+ * POST a JSON encoded $content to a $url with HTTP/1.0 and return TRUE.
  *
  * @param string $url POSTed to
- * @param int $request JSON object to POST
- * @param in $timeout of the request, defaults to 0.05 seconds
+ * @param string $content of the encoded JSON object to POST
+ * @param in $timeout of the request, defaults to UNFRAMED_CAST_TIMEOUT
  *
- * @return string the response's body
+ * @return boolean
  */
-function unframed_cast ($url, $request, $timeout=0.05) {
-    $content = json_encode($request);
+function unframed_cast_encoded ($url, $content, $timeout=UNFRAMED_CAST_TIMEOUT) {
     $context = stream_context_create(array(
         'http' => array(
             'protocol_version'=>'1.0',
@@ -46,7 +41,20 @@ function unframed_cast ($url, $request, $timeout=0.05) {
             )
         ));
     @file_get_contents($url, false, $context);
-    return True;
+    return TRUE;
+}
+
+/**
+ * Cast a $json request to a $url with HTTP/1.0 and return TRUE.
+ *
+ * @param string $url POSTed to
+ * @param string $json JSON object to POST
+ * @param in $timeout of the request, defaults to UNFRAMED_CAST_TIMEOUT
+ *
+ * @return boolean
+ */
+function unframed_cast ($url, $json, $timeout=UNFRAMED_CAST_TIMEOUT) {
+    return unframed_cast_encoded($url, json_encode($json), $timeout);
 }
 
 // How to handle a local cast message
@@ -71,14 +79,14 @@ function unframed_cast_ok () {
  * @param int $options JSON options, default to 0
  *
  * @return UnframedMessage
- * @throws Unframed 
+ * @throws Unframed
  */
 function unframed_cast_receive ($maxLength=16384, $maxDepth=512, $options=0) {
-    $remote = $_SERVER['REMOTE_ADDR']; 
+    $remote = $_SERVER['REMOTE_ADDR'];
     if (!($remote == '127.0.0.1' || $remote == $_SERVER['SERVER_ADDR'])) {
-        throw new Unframed('Unauthorized '.$_SERVER['REMOTE_ADDR'], 403);    
+        throw new Unframed('Unauthorized '.$_SERVER['REMOTE_ADDR'], 403);
     }
-    return unframed_post_json_body($maxLength, $maxDepth, $options);    
+    return unframed_post_json_body($maxLength, $maxDepth, $options);
 }
 
 /**
@@ -88,7 +96,7 @@ function unframed_cast_receive ($maxLength=16384, $maxDepth=512, $options=0) {
  * @param int $maxLength of the JSON request body, defaults to 16384 bytes
  * @param in $maxDepth of the JSON request, defaults to 512
  *
- * @throws Unframed 
+ * @throws Unframed
  */
 function unframed_cast_json ($fun, $maxLength=16384, $maxDepth=512) {
     try {
@@ -105,38 +113,96 @@ function unframed_cast_json ($fun, $maxLength=16384, $maxDepth=512) {
 
 // test
 
-function unframed_cast_test_get ($message) {
-    $timeout = $message->asFloat('timeout', UNFRAMED_CAST_TIMEOUT);
-    $sleep = $message->asFloat('sleep', UNFRAMED_CAST_SLEEP);
-    touch('.unframed_cast_test');
-    $time = time();
-    unframed_cast(unframed_cast_url(), $message->array, $timeout);
-    sleep($sleep + 1);
-    return array(
-        'pass' => !file_exists('.unframed_cast_test'),
-        'slept' => time() - $time
-        );
+function unframed_cast_test_post ($message) {
+    // sleep ...
+    $sleep = $message->asFloat('sleep');
+    sleep($sleep);
+    // ... then either ...
+    if ($message->has('semaphore')) {
+        // ... unlink a semaphore.
+        $semaphore = $message->getString('semaphore');
+        unlink($semaphore);
+    } elseif ($message->has('configure')) {
+        // or (re)configure.
+        $configuration = $message->getMessage('configure');
+        unframed_configure (
+            $configuration->asInt('concurrent', 8),
+            $configuration->asFloat('timeout', 0.03),
+            $sleep
+            );
+    }
 }
 
-function unframed_cast_test_post ($message) {
-    $sleep = $message->asFloat('sleep', UNFRAMED_CAST_SLEEP);
-    $timeout = $message->asFloat('timeout', UNFRAMED_CAST_TIMEOUT);
-    sleep($sleep - $timeout);
-    unlink('.unframed_cast_test');
+function unframed_cast_test_semaphore ($slug, $index) {
+    return dirname(__FILE__).'/.unframed_cast_test_'.$slug.'-'.$index;
+}
+
+function unframed_cast_test_timeout ($ms) {
+    return !file_exists(unframed_cast_test_semaphore('timeout', $ms));
+}
+
+function unframed_cast_test_timeouts ($sleep, $timeouts) {
+    foreach($timeouts as $timeout) {
+        $semaphore = unframed_cast_test_semaphore('timeout', $timeout);
+        touch($semaphore);
+        unframed_cast(unframed_cast_url(), array(
+            'sleep' => ($sleep-($timeout/1000)),
+            'semaphore' => $semaphore
+            ), $timeout/1000);
+    }
+    sleep($sleep + array_sum($timeouts)/1000 + 1);
+    return array_filter($timeouts, 'unframed_cast_test_timeout');
+}
+
+function unframed_cast_test_sleeps ($sleeps, $timeout, $concurrent) {
+    foreach ($sleeps as $sleep) {
+        unframed_cast(unframed_cast_url(), array(
+            'sleep' => $sleep,
+            'configure' => array(
+                'concurrent' => $concurrent,
+                'timeout' => $timeout
+                ),
+            ), $timeout);
+    }
+}
+
+function unframed_cast_test_get ($message) {
+    $sleep = $message->getFloat('sleep', 2.0);
+    $timeouts = $message->getFloatArray('timeouts', array(
+        0.1, 0.2, 0.4, 0.8, 1, 2, 3, 4
+        )); // in ms
+    $sleeps = $message->getFloatArray('sleeps', array(
+        5, 10, 15, 30, 45, 90, 135, 270
+        )); // in seconds
+    $concurrents = unframed_cast_test_timeouts($sleep, $timeouts);
+    $concurrency = count($concurrents);
+    if ($concurrency > 0) {
+        $timeout = (array_sum($concurrents) / $concurrency) / 1000;
+        unframed_cast_test_sleeps(
+            $sleeps, $timeout, $concurrency
+            );
+        return array(
+            'pass' => TRUE,
+            'concurrent' => $concurrency,
+            'timeout' => $timeout
+            );
+    } else {
+        return array(
+            'pass' => FALSE
+            );
+    }
 }
 
 /**
  * A test script for cast, with variable timeout and sleep times.
  */
-function unframed_cast_json_test ($timeout=0.5, $sleep=3) {
-    define('UNFRAMED_CAST_TIMEOUT', $timeout);
-    define('UNFRAMED_CAST_SLEEP', $sleep);
+function unframed_cast_test_json ($fun='unframed_cast_test_get') {
     $method = $_SERVER['REQUEST_METHOD'];
-    if ($method == 'GET') { // Send cast message
-        unframed_get_json ('unframed_cast_test_get');
-    } elseif ($method == 'POST') { // Receive cast message
+    if ($method == 'GET') { // Send cast messages
+        unframed_get_json ($fun);
+    } elseif ($method == 'POST') { // Receive cast messages
         unframed_cast_json('unframed_cast_test_post');
-    } else { // 405
+    } else {
         unframed_json_error(new Unframed('Invalid Method', 405));
     }
 }
